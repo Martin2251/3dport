54601783e66273ed968a6854022b627fc9639cbc
Martin Lowe <martinlowe673@gmail.com>
2025-04-23 07:53:18 +0100

    classname

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const Words = [
+    {text:"Ideas", imgPath:"/images/ideas.svg"},
+    {text:"Concepts", imgPath:"/images/concepts.svg"},
+]
+
 const Hero = () => {
   return (
    <section id="hero" className='relative overflow-hidden'>
@@ -11,8 +16,26 @@ const Hero = () => {
             <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                 <div className='flex flex-col gap-7'>
                     <div className='hero-text'>
+                    <h1>Shaping 
+                    <span className='slide'>
+                        <span className='wrapper'>
+                            {Words.map((word) =>(
+                                <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                <img src={word.imgPath} 
+                                alt={word.text}
+                                className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                                
+                                />
 
-                        
+                                </span>
+                            ))}
+                        </span>
+                    </span>
+
+                    </h1>
+                    <h1>intro Real Projects</h1>
+                    <h1>that deliver results</h1>
+
                     </div>
                 </div>
             </header>
